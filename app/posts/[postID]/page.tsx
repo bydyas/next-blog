44136b7616338fefc810a33ffa88dea5738a35dd
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2023-08-25 12:48:38 +0300

    Render posts on page

--- app/posts/[postID]/page.tsx
+++ app/posts/[postID]/page.tsx
@@ -1,5 +1,7 @@
-import { getSortedPostsData } from '@/lib/posts';
+import getFormattedDate from '@/lib/getFormattedDate';
+import { getPostData, getSortedPostsData } from '@/lib/posts';
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
 
 export async function generateMetadata({ params }: { params: { postID: string } }) {
   const posts = getSortedPostsData();
@@ -24,5 +26,20 @@ export default async function Post({ params }: { params: { postID: string } }) {
 
   if (!posts.find((post) => post.id === postID)) return notFound();
 
-  return <div>page</div>;
+  const { title, date, contentHtml } = await getPostData(postID);
+
+  const pubDate = getFormattedDate(date);
+
+  return (
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/">← Back to home</Link>
+        </p>
+      </article>
+    </main>
+  );
 }
